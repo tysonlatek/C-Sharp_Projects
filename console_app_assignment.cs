9d9f19db1449505ea89989741b44f1358c68de4d
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-01 02:26:32 -0400

    Create console_app_assignment.cs

---
+++ console_app_assignment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Application_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number:");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            bool isEight = guess == 8;
+
+            do
+            {
+                switch (guess)
+                {
+                    case 7:
+                        Console.WriteLine("So close!");
+                        Console.WriteLine("Guess a number:");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8:
+                        Console.WriteLine("You guessed it!");
+                        isEight = true;
+                        break;
+                    case 9:
+                        Console.WriteLine("So close!");
+                        Console.WriteLine("Guess a number:");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Try again.");
+                        Console.WriteLine("Guess a number:");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            while (!isEight);
+
+            Console.Read();
+        }
+    }
+}
