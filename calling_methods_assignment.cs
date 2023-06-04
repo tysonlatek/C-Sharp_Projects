b11651330831f6b978b4cafe38e73b72a1048914
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-04 04:34:06 -0400

    Create calling_methods_assignment.cs

---
+++ calling_methods_assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int numAdd = Convert.ToInt32(Console.ReadLine());
+            Calculator.Addition(numAdd, 10);
+            Console.WriteLine("\nPlease enter a number:");
+            int numSub = Convert.ToInt32(Console.ReadLine());
+            Calculator.Subtraction(numSub, 10);
+            Console.WriteLine("\nPlease enter a number:");
+            int numMul = Convert.ToInt32(Console.ReadLine());
+            Calculator.Multiplication(numMul, 10);
+            Console.ReadLine();
+        }
+    }
+}
