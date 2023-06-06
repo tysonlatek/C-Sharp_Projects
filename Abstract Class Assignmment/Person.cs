aaa2eecf0b684cfbdaa10f777b73ec803dd3a523
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-06 17:24:42 -0400

    Created Abstract Class Assignment

---
+++ Abstract Class Assignmment/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public abstract class Person
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public abstract void SayName();
+    }
+}
