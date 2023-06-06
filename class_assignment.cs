116e02d60995e2531aad954bcc707017205bfacd
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-06 02:21:55 -0400

    Create class_assignment.cs

---
+++ class_assignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator divide = new Calculator();
+            Calculator.Divide();
+            
+            int c;
+            float d;
+            Calculator output = new Calculator();
+            Calculator.Output(out c);
+            Calculator.Output(out d);
+            Console.ReadLine();
+        }
+    }
+}
