dbc909b66e8d4bc52cab55b070ca00d275100f80
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-06 00:52:38 -0400

    Create method_assignment.cs

---
+++ method_assignment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator multiply = new Calculator();
+            Console.WriteLine("Please enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int optNum;
+            if (int.TryParse(Console.ReadLine(), out optNum))
+            {
+                Calculator.Multiply(num, optNum);
+            }
+            else
+            {
+                Calculator.Multiply(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
