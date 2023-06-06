224287bde3958d324603dc861e4275928f85f0c3
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-06 15:39:48 -0400

    Created the Methods and Objects Assignment

---
+++ Methods and Objects Assignment/methods_and_objects_assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { FirstName = "John", LastName = "Smith" };
+            person.SayName();
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
