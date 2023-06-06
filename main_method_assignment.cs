4a825d2525572ace81452156f8f713a3184fb6dd
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-05 23:12:25 -0400

    Create main_method_assignment.cs

---
+++ main_method_assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator multiplication = new Calculator();
+            Calculator.MathOp(5, 10);
+
+            Calculator division = new Calculator();
+            Calculator.MathOp(9.80f, 15);
+
+            Calculator addition = new Calculator();
+            Calculator.MathOp("15", 20);
+
+            Console.ReadLine();
+        }
+    }
+}
