8464264ac8bb78af16f87a31506e63c9e3360b2a
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-06 21:10:50 -0400

    Create operators_assignment.cs

---
+++ operators_assignment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee id1 = new Employee(123);
+            Employee id2 = new Employee(321);
+            if (id1 == id2)
+            {
+                Console.WriteLine("The IDs are identical.");
+            }
+            else
+            {
+                Console.WriteLine("The IDs are different.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
