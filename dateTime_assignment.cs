7a94d7c399e5cd34106736e72915a6f931ac59ab
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-06-13 22:31:11 -0400

    Create dateTime_assignment.cs

---
+++ dateTime_assignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Please enter a number:");
+            int input = int.Parse(Console.ReadLine());
+            DateTime future = now.AddHours(input);
+            Console.WriteLine("In {0} hours it will be {1}", input, future);
+            Console.ReadLine();
+        }
+    }
+}
